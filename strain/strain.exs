d353b9a0a87979b4656ca72fbe59868b21c5d2fa
Gabriel Giordano <git@gabrielgiordano.com>
2019-02-16 21:42:27 -0200

    Refactor strain solution

--- strain/strain.exs
+++ strain/strain.exs
@@ -6,17 +6,9 @@ defmodule Strain do
   Do not use `Enum.filter`.
   """
   @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
-  def keep(list, fun) do
-    do_keep(list, fun)
-  end
-
-  defp do_keep([h | t], f) do
-    if f.(h), do: [h | do_keep(t, f)], else: do_keep(t, f)
-  end
-
-  defp do_keep([], f) do
-    []
-  end
+  def keep(list, fun), do: do_keep(list, fun)
+  defp do_keep([h | t], f), do: if f.(h), do: [h | do_keep(t, f)], else: do_keep(t, f)
+  defp do_keep([], f), do: []
 
   @doc """
   Given a `list` of items and a function `fun`, return the list of items where
@@ -25,15 +17,7 @@ defmodule Strain do
   Do not use `Enum.reject`.
   """
   @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
-  def discard(list, fun) do
-    do_discard(list, fun)
-  end
-
-  defp do_discard([h | t], f) do
-    unless f.(h), do: [h | do_discard(t, f)], else: do_discard(t, f)
-  end
-
-  defp do_discard([], f) do
-    []
-  end
+  def discard(list, fun), do: do_discard(list, fun)
+  defp do_discard([h | t], f), do: unless f.(h), do: [h | do_discard(t, f)], else: do_discard(t, f)
+  defp do_discard([], f), do: []
 end
