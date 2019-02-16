2a0aa918a7871f61c5cdb669cdc7bacd1ed8c7fb
Gabriel Giordano <git@gabrielgiordano.com>
2019-02-16 21:34:04 -0200

    Add protein-translation solution

---
+++ protein-translation/protein_translation.exs
@@ -0,0 +1,68 @@
+defmodule ProteinTranslation do
+  @codons %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP"
+  }
+  @invalid_codon "invalid codon"
+  @invalid_rna "invalid RNA"
+
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: {atom, list(String.t())}
+  def of_rna(rna), do: of_rna(rna, [])
+
+  defp of_rna("", []), do: { :error, @invalid_rna }
+  defp of_rna("", proteins), do: { :ok, proteins }
+  defp of_rna(<<codon::binary-size(3), tail::binary>>, proteins) do
+    case of_codon(codon) do
+      { :ok, "STOP" } -> of_rna("", proteins)
+      { :ok, protein } -> of_rna(tail, proteins ++ [protein])
+      { :error, _ } -> of_rna("", [])
+    end
+  end
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: {atom, String.t()}
+  def of_codon(codon) do
+    case @codons[codon] do
+      nil -> { :error, @invalid_codon }
+      protein -> { :ok, protein }
+    end
+  end
+end
