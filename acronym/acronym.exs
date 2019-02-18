b8893f25ddfbe26a8c2b99b1d383f1874a5ffdbc
Gabriel Giordano <git@gabrielgiordano.com>
2019-02-18 17:30:54 -0300

    Small improvement on acronym solution

--- acronym/acronym.exs
+++ acronym/acronym.exs
@@ -4,10 +4,10 @@ defmodule Acronym do
   "This is a string" => "TIAS"
   """
   @spec abbreviate(String.t()) :: String.t()
-  def abbreviate(s), do: s |> do_abbreviate() |> to_string()
+  def abbreviate(s), do: s |> do_abbreviate() |> IO.iodata_to_binary
 
   defp do_abbreviate(<<f, s, t::bits>>) when f in ' ,-' and s >= ?a and s <= ?z, do: [s - 32 | do_abbreviate(t)]
   defp do_abbreviate(<<f, t::bits>>) when f >= ?A and f <= ?Z, do: [f | do_abbreviate(t)]
   defp do_abbreviate(<<_, t::bits>>), do: do_abbreviate(t)
-  defp do_abbreviate(""), do: ''
+  defp do_abbreviate(<<>>), do: <<>>
 end
